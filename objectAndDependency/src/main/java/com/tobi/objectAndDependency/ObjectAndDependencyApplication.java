1aae5d04763b4bf39d7a88a8e2a913815466c86f
suha <hp980724@gmail.com>
2023-06-05 13:46:09 +0900

    Initial commit

---
+++ objectAndDependency/src/main/java/com/tobi/objectAndDependency/ObjectAndDependencyApplication.java
@@ -0,0 +1,13 @@
+package com.tobi.objectAndDependency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObjectAndDependencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObjectAndDependencyApplication.class, args);
+	}
+
+}
